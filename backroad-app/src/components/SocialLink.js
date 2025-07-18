9569fac900b4e67dea44bdc344cb01ffa83ddacb
konyatomi <konyatomi16@gmail.com>
2025-07-19 00:18:25 +0200

    noreferrer

--- backroad-app/src/components/SocialLink.js
+++ backroad-app/src/components/SocialLink.js
@@ -1,7 +1,7 @@
 const SocialLink = ({ href, icon, itemClass }) => {
   return (
     <li>
-      <a href={href} target="_blank" rel="noopener" className={itemClass}>
+      <a href={href} target="_blank" rel="noreferrer" className={itemClass}>
         <i className={icon}></i>
       </a>
     </li>
