9b723f88d54adaafd60018f301cde107d9f94309
konyatomi <konyatomi16@gmail.com>
2025-07-18 22:43:57 +0200

    Title Component

--- backroad-app/src/components/Services.js
+++ backroad-app/src/components/Services.js
@@ -1,45 +1,51 @@
+import Title from "./Title";
+
 const Services = () => {
   return (
     <section className="section services" id="services">
-        <div className="section-title">
-        <h2>our <span>services</span></h2>
-        </div>
-        <div className="section-center services-center">
+      <Title title="our" subtitle="services" />
+      <div className="section-center services-center">
         <article className="service">
-            <span className="service-icon"><i className="fas fa-wallet fa-fw"></i></span>
-            <div className="service-info">
+          <span className="service-icon">
+            <i className="fas fa-wallet fa-fw"></i>
+          </span>
+          <div className="service-info">
             <h4 className="service-title">saving money</h4>
             <p className="service-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, officia.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores, officia.
             </p>
-            </div>
+          </div>
         </article>
 
         <article className="service">
-            <span className="service-icon"><i className="fas fa-tree fa-fw"></i></span>
-            <div className="service-info">
+          <span className="service-icon">
+            <i className="fas fa-tree fa-fw"></i>
+          </span>
+          <div className="service-info">
             <h4 className="service-title">endless hiking</h4>
             <p className="service-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, officia.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores, officia.
             </p>
-            </div>
+          </div>
         </article>
 
         <article className="service">
-            <span className="service-icon"><i className="fas fa-socks fa-fw"></i></span>
-            <div className="service-info">
+          <span className="service-icon">
+            <i className="fas fa-socks fa-fw"></i>
+          </span>
+          <div className="service-info">
             <h4 className="service-title">amazing comfort</h4>
             <p className="service-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, officia.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores, officia.
             </p>
-            </div>
+          </div>
         </article>
-        </div>
+      </div>
     </section>
-  )
+  );
 };
 
 export default Services;
