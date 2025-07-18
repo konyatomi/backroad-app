2372430ea86e874ab410aaa7b0cc33794a6432ce
konyatomi <konyatomi16@gmail.com>
2025-07-18 16:44:14 +0200

    Social Links

--- backroad-app/src/components/Navbar.js
+++ backroad-app/src/components/Navbar.js
@@ -1,47 +1,52 @@
-import logo from '../images/logo.svg';
-import { pageLinks } from '../data';
+import logo from "../images/logo.svg";
+import { pageLinks } from "../data";
+import { scocialLinks } from "../data";
 
 const Navbar = () => {
-    return(
-        <nav className="navbar">
-            <div className="nav-center">
-            <div className="nav-header">
-                <img src={logo} className="nav-logo" alt="backroads" />
-                <button type="button" className="nav-toggle" id="nav-toggle">
-                <i className="fas fa-bars"></i>
-                </button>
-            </div>
-            {/* <!-- left this comment on purpose --> */}
-            <ul className="nav-links" id="nav-links">
-                {pageLinks.map((link) => {
-                    return (
-                        <li key={link.id}>
-                            <a href={link.href} className="nav-link"> {link.text} </a>
-                        </li>
-                    )
-                })}
-            </ul>
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        {/* <!-- left this comment on purpose --> */}
+        <ul className="nav-links" id="nav-links">
+          {pageLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} className="nav-link">
+                  {" "}
+                  {link.text}{" "}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
 
-            <ul className="nav-icons">
-                <li>
-                <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-                    ><i className="fab fa-facebook"></i
-                ></a>
-                </li>
-                <li>
-                <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-                    ><i className="fab fa-twitter"></i
-                ></a>
-                </li>
-                <li>
-                <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-                    ><i className="fab fa-squarespace"></i
-                ></a>
-                </li>
-            </ul>
-            </div>
-        </nav>
-    )
+        <ul className="nav-icons">
+          {scocialLinks.map((link) => {
+            const { id, href, icon } = link;
+
+            return (
+              <li key={id}>
+                <a
+                  href={href}
+                  target="_blank"
+                  rel="noopener"
+                  className="nav-icon"
+                >
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
