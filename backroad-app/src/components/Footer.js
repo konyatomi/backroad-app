441940a0325da53abd546a2f5c45de8a4446280b
konyatomi <konyatomi16@gmail.com>
2025-07-19 00:07:01 +0200

    warnings

--- backroad-app/src/components/Footer.js
+++ backroad-app/src/components/Footer.js
@@ -21,7 +21,6 @@ const Footer = () => {
       </ul> */}
       <ul className="footer-icons">
         {socialLinks.map((link) => {
-          const { id, href, icon } = link;
           return <SocialLink key={link.id} {...link} itemClass="footer-icon" />;
         })}
       </ul>
